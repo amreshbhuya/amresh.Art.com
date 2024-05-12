02b719eef945e35b44681e53f010a8ab0977e858
amreshbhuya <158759279+amreshbhuya@users.noreply.github.com>
2024-05-12 12:12:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,496 @@
+
+
+    // Artwork data for Mausumi Bhuyan
+    const artworks = [
+        {
+            name: 'Artwork 1 by Mausumi Bhuyan',
+            price: '$150',
+            image: 'https://i.ibb.co/1dhvTC2/Whats-App-Image-2024-03-23-at-20-19-38-75fab908.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 2 by Mausumi Bhuyan',
+            price: '$180',
+            image: 'https://i.ibb.co/8smbjRf/Whats-App-Image-2024-03-23-at-20-20-07-3f12df8e.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 3 by Mausumi Bhuyan',
+            price: '$200',
+            image: 'https://i.ibb.co/kJ0SSLR/Whats-App-Image-2024-03-23-at-20-20-06-78b9fb25.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 4 by Mausumi Bhuyan',
+            price: '$220',
+            image: 'https://i.ibb.co/DtZKnY1/Whats-App-Image-2024-03-23-at-20-20-04-08b3a1e6.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 5 by Mausumi Bhuyan',
+            price: '$250',
+            image: 'https://i.ibb.co/Wy10SF2/Whats-App-Image-2024-03-23-at-20-19-33-a96e9560.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 6 by Mausumi Bhuyan',
+            price: '$280',
+            image: 'https://i.ibb.co/qsvMM4N/Whats-App-Image-2024-03-23-at-20-19-32-87a1c39f.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 7 by Mausumi Bhuyan',
+            price: '$300',
+            image: 'https://i.ibb.co/Jp2DtKd/Whats-App-Image-2024-03-23-at-20-19-33-220b0583.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 8 by Mausumi Bhuyan',
+            price: '$350',
+            image: 'IMG-20240512-WA0001.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 9 by Mausumi Bhuyan',
+            price: '$380',
+            image: 'IMG-20240512-WA0002.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 10 by Mausumi Bhuyan',
+            price: '$400',
+            image: 'IMG-20240512-WA0003.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 11 by Mausumi Bhuyan',
+            price: '$420',
+            image: 'IMG-20240512-WA0004.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 12 by Mausumi Bhuyan',
+            price: '$450',
+            image: 'IMG-20240512-WA0005.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 13 by Mausumi Bhuyan',
+            price: '$480',
+            image: 'IMG-20240512-WA0006.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 14 by Mausumi Bhuyan',
+            price: '$500',
+            image: 'IMG-20240512-WA0007.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 15 by Mausumi Bhuyan',
+            price: '$520',
+            image: 'IMG-20240512-WA0008.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 16 by Mausumi Bhuyan',
+            price: '$550',
+            image: 'IMG-20240512-WA0009.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 17 by Mausumi Bhuyan',
+            price: '$580',
+            image: 'IMG-20240512-WA0010.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 18 by Mausumi Bhuyan',
+            price: '$600',
+            image: 'IMG-20240512-WA0011.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 19 by Mausumi Bhuyan',
+            price: '$620',
+            image: 'IMG-20240512-WA0012.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 20 by Mausumi Bhuyan',
+            price: '$650',
+            image: 'IMG-20240512-WA0013.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 21 by Mausumi Bhuyan',
+            price: '$680',
+            image: 'IMG-20240512-WA0014.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 22 by Mausumi Bhuyan',
+            price: '$700',
+            image: 'IMG-20240512-WA0015.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 23 by Mausumi Bhuyan',
+            price: '$720',
+            image: 'IMG-20240512-WA0016.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 24 by Mausumi Bhuyan',
+            price: '$750',
+            image: 'IMG-20240512-WA0017.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 25 by Mausumi Bhuyan',
+            price: '$780',
+            image: 'IMG-20240512-WA0018.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 26 by Mausumi Bhuyan',
+            price: '$800',
+            image: 'IMG-20240512-WA0019.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 27 by Mausumi Bhuyan',
+            price: '$820',
+            image: 'IMG-20240512-WA0020.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 28 by Mausumi Bhuyan',
+            price: '$850',
+            image: 'IMG-20240512-WA0021.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 29 by Mausumi Bhuyan',
+            price: '$880',
+            image: 'IMG-20240512-WA0022.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 30 by Mausumi Bhuyan',
+            price: '$900',
+            image: 'IMG-20240512-WA0023.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 31 by Mausumi Bhuyan',
+            price: '$920',
+            image: 'IMG-20240512-WA0024.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 32 by Mausumi Bhuyan',
+            price: '$950',
+            image: 'IMG-20240512-WA0025.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 33 by Mausumi Bhuyan',
+            price: '$980',
+            image: 'IMG-20240512-WA0026.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 34 by Mausumi Bhuyan',
+            price: '$1000',
+            image: 'IMG-20240512-WA0027.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 35 by Mausumi Bhuyan',
+            price: '$1020',
+            image: 'IMG-20240512-WA0028.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 36 by Mausumi Bhuyan',
+            price: '$1050',
+            image: 'IMG-20240512-WA0029.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 37 by Mausumi Bhuyan',
+            price: '$1080',
+            image: 'IMG-20240512-WA0030.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 38 by Mausumi Bhuyan',
+            price: '$1100',
+            image: 'IMG-20240512-WA0031.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 39 by Mausumi Bhuyan',
+            price: '$1120',
+            image: 'IMG-20240512-WA0032.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 40 by Mausumi Bhuyan',
+            price: '$1150',
+            image: 'IMG-20240512-WA0033.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 41 by Mausumi Bhuyan',
+            price: '$1180',
+            image: 'IMG-20240512-WA0034.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 42 by Mausumi Bhuyan',
+            price: '$1200',
+            image: 'IMG-20240512-WA0035.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 43 by Mausumi Bhuyan',
+            price: '$1220',
+            image: 'IMG-20240512-WA0036.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 44 by Mausumi Bhuyan',
+            price: '$1250',
+            image: 'IMG-20240512-WA0037.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 45 by Mausumi Bhuyan',
+            price: '$1280',
+            image: 'IMG-20240512-WA0038.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 46 by Mausumi Bhuyan',
+            price: '$1300',
+            image: 'IMG-20240512-WA0039.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 47 by Mausumi Bhuyan',
+            price: '$1320',
+            image: 'IMG-20240512-WA0040.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 48 by Mausumi Bhuyan',
+            price: '$1350',
+            image: 'IMG-20240512-WA0041.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 49 by Mausumi Bhuyan',
+            price: '$1380',
+            image: 'IMG-20240512-WA0042.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 50 by Mausumi Bhuyan',
+            price: '$1400',
+            image: 'IMG-20240512-WA0043.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 51 by Mausumi Bhuyan',
+            price: '$1420',
+            image: 'IMG-20240512-WA0044.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 52 by Mausumi Bhuyan',
+            price: '$1450',
+            image: 'IMG-20240512-WA0045.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 53 by Mausumi Bhuyan',
+            price: '$1480',
+            image: 'IMG-20240512-WA0046.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 54 by Mausumi Bhuyan',
+            price: '$1500',
+            image: 'IMG-20240512-WA0047.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 55 by Mausumi Bhuyan',
+            price: '$1520',
+            image: 'IMG-20240512-WA0048.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 56 by Mausumi Bhuyan',
+            price: '$1550',
+            image: 'IMG-20240512-WA0049.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 57 by Mausumi Bhuyan',
+            price: '$1580',
+            image: 'IMG-20240512-WA0050.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 58 by Mausumi Bhuyan',
+            price: '$1600',
+            image: 'IMG-20240512-WA0051.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 59 by Mausumi Bhuyan',
+            price: '$1620',
+            image: 'IMG-20240512-WA0052.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 60 by Mausumi Bhuyan',
+            price: '$1650',
+            image: 'IMG-20240512-WA0053.jpg',
+            category: 'Hand Art'
+        },
+        {
+            name: 'Artwork 61 by Mausumi Bhuyan',
+            price: '$1680',
+            image: 'IMG-20240512-WA0054.jpg',
+            category: 'Hand Art'
+        }
+    ];
+
+    const cart = [];
+
+    function displayArtworks() {
+        const galleryContainer = document.getElementById('galleryContainer');
+        galleryContainer.innerHTML = '';
+        artworks.forEach(artwork => {
+            const artworkElement = document.createElement('div');
+            artworkElement.classList.add('artwork');
+            artworkElement.innerHTML = `
+                <img src="${artwork.image}" alt="${artwork.name}">
+                <div class="artwork-details">
+                    <h3 class="artwork-name">${artwork.name}</h3>
+                    <p class="artwork-price">${artwork.price}</p>
+                    <p class="artwork-category">${artwork.category}</p>
+                    <button class="add-to-cart-btn" onclick="addToCart('${artwork.name}', '${artwork.price}', '${artwork.image}')">Add to Cart 🛒</button>
+                </div>
+            `;
+            galleryContainer.appendChild(artworkElement);
+        });
+    }
+
+    function addToCart(name, price, image) {
+        cart.push({ name, price, image });
+        updateCart();
+    }
+
+    function removeFromCart(index) {
+        cart.splice(index, 1);
+        updateCart();
+    }
+
+    function updateCart() {
+        const cartItems = document.getElementById('cartItems');
+        const cartTotal = document.getElementById('cartTotal');
+        cartItems.innerHTML = '';
+        let total = 0;
+        cart.forEach((item, index) => {
+            const cartItem = document.createElement('div');
+            cartItem.classList.add('cart-item');
+            cartItem.innerHTML = `
+                <img src="${item.image}" alt="${item.name}">
+                <div>
+                    <p class="cart-item-name">${item.name}</p>
+                    <p>${item.price}</p>
+                </div>
+                <button class="cart-item-remove" onclick="removeFromCart(${index})">Remove</button>
+            `;
+            cartItems.appendChild(cartItem);
+            total += parseInt(item.price.substring(1));
+        });
+        cartTotal.textContent = `Total: $${total}`;
+    }
+
+
+    function searchArtworks() {
+        const searchInput = document.getElementById('searchInput').value.toLowerCase();
+        const filteredArtworks = artworks.filter((artwork) => artwork.name.toLowerCase().includes(searchInput));
+        displayArtworks(filteredArtworks);
+    }
+
+    const users = [
+        { name: "Amresh Bhuyan", email: "amreshbhuyanone@gmail.com", password: "Amresh@1234" },
+        { name: "Jane Smith", email: "jane@example.com", password: "password456" }
+        ];
+        
+        let currentUser = null;
+        
+        function handleSignup(event) {
+        event.preventDefault();
+        const name = document.getElementById('signupName').value.trim();
+        const email = document.getElementById('signupEmail').value.trim();
+        const password = document.getElementById('signupPassword').value.trim();
+        
+        const userExists = users.find(user => user.email === email);
+        if (userExists) {
+            alert("Email is already registered. Please log in.");
+            return;
+        }
+        
+        users.push({ name, email, password });
+        currentUser = { name, email };
+        showUserProfile();
+        }
+        
+        function handleLogin(event) {
+        event.preventDefault();
+        const email = document.getElementById('loginEmail').value.trim();
+        const password = document.getElementById('loginPassword').value.trim();
+        
+        const user = users.find(user => user.email === email && user.password === password);
+        if (user) {
+            currentUser = user;
+            showUserProfile();
+        } else {
+            alert("Invalid email or password");
+        }
+        }
+        
+        function logout() {
+        currentUser = null;
+        showLoginForm();
+        }
+        
+        function showSignupForm() {
+        document.getElementById('signupForm').style.display = 'block';
+        document.getElementById('loginForm').style.display = 'none';
+        }
+        
+        function showLoginForm() {
+        document.getElementById('signupForm').style.display = 'none';
+        document.getElementById('loginForm').style.display = 'block';
+        }
+        
+        function showUserProfile() {
+        document.getElementById('userName').textContent = currentUser.name;
+        document.getElementById('userProfile').style.display = 'block';
+        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('signupForm').style.display = 'none';
+        }
+
+        
+    window.onload = displayArtworks;
+
+   
